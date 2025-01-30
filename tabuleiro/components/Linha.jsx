7d5da5dfcd9d76bee2e0b4baab11281bc2f5f60e
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2025-01-29 21:32:32 -0300

    criando um tabuleiro em react

---
+++ tabuleiro/components/Linha.jsx
@@ -0,0 +1,16 @@
+import Casa from "./Casa";
+import styles from '../styles/Linha.module.css'
+export default function Linha(props){
+    return(
+        <div className={styles.linha}>
+            <Casa preta ={props.preta}/>
+            <Casa preta ={!props.preta}/>
+            <Casa preta ={props.preta}/>
+            <Casa preta ={!props.preta}/>
+            <Casa preta ={props.preta}/>
+            <Casa preta ={!props.preta}/>
+            <Casa preta ={props.preta}/>
+            <Casa preta ={!props.preta}/>
+        </div>
+    )
+}
---
+++ tabuleiro/pages/index.jsx
@@ -0,0 +1,14 @@
+
+import Tabuleiro from "@/components/Tabuleiro";
+import styles from "../styles/Home.module.css";
+
+
+
+
+export default function Home() {
+  return (
+  <div className={styles.container}>
+  <Tabuleiro/>
+  </div>
+  )
+}
